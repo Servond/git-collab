4be7140fbc0ab65b8de9241ad5e9459c2e47e10c
Bryan Karta <65345089+Servond@users.noreply.github.com>
2024-12-04 17:06:18 +0700

    feat(role): update role (#4)

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -1,6 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { findUserService } from "../services/article.service";
 
+const getarticle = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { title, body } = req.body;
+
+    const users = await findUserService();
+    res;
+  } catch (err) {
+    const error = err as Error;
+    next(err);
+  }
+};
+
 const CreateArticle = async (
   req: Request,
   res: Response,
